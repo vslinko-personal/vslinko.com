ea826c62f95563e26397a2e3540aeed48df656e8
Viacheslav Slinko <vslinko@yahoo.com>
2021-04-11 12:22:42 +0300

    Configured cache for devserver

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ async function main() {
 
   const app = express();
 
-  app.use(express.static("dist"));
+  app.use(
+    express.static("dist", {
+      cacheControl: true,
+      maxAge: "10m",
+      immutable: true,
+    })
+  );
 
   app.listen(3000, "0.0.0.0", () => {
     console.log("Listening 0.0.0.0:3000");
